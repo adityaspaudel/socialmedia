ea7286e5d3cc48b2e1394f870ba556d657069d76
Aaditya Paudel <78781844+adityaspaudel@users.noreply.github.com>
2024-10-24 08:31:17 +0545

    video-player made

---
+++ nextjs/src/app/demo/page.js
@@ -0,0 +1,13 @@
+"use client";
+import VideoPlayer from "@/components/videoplayer/page";
+import React from "react";
+
+const Demo = () => {
+	return (
+		<div>
+			<VideoPlayer />
+		</div>
+	);
+};
+
+export default Demo;
--- nextjs/src/components/videoplayer/page.js
+++ nextjs/src/components/videoplayer/page.js
@@ -1,9 +1,26 @@
+"use client";
+
 import React from "react";
 
 const VideoPlayer = () => {
-	function previousTrack() {}
-	function playPauseTrack() {}
-	function nextTrack() {}
+	function previousTrack() {
+		alert("previousTrack");
+		document.getElementsByClassName("vidSource")[0].src = "vid-2.mp4";
+		document.getElementsByClassName("vidSource")[0].type = "video/mp4";
+		document.getElementsByClassName("video-player")[0].load();
+		document.getElementsByClassName("video-player")[0].play();
+	}
+	function playPauseTrack() {
+		alert("play/ pause");
+		document.getElementsByClassName("video-player")[0].pause();
+	}
+	function nextTrack() {
+		alert("nextTrack");
+		document.getElementsByClassName("vidSource")[0].src = "vid-4.mp4";
+		document.getElementsByClassName("vidSource")[0].type = "video/mp4";
+		document.getElementsByClassName("video-player")[0].load();
+		document.getElementsByClassName("video-player")[0].play();
+	}
 	return (
 		<div className="m-auto w-[60%] flex flex-col">
 			<h1>this is a VideoPlayer</h1>
@@ -15,7 +32,8 @@ const VideoPlayer = () => {
 				controls
 				autoPlay>
 				<source
-					src="vid-4.mp4"
+					className="vidSource"
+					src="vid-3.mp4"
 					type="video/mp4"
 				/>
 				{/* <source
@@ -33,17 +51,17 @@ const VideoPlayer = () => {
 			</video>
 			<div className="flex gap-4 musicPlayButtons ">
 				<button
-					className="previousTrack"
+					className="previousTrack bg-gray-200 p-2 rounded-md hover:bg-gray-300"
 					onClick={previousTrack}>
 					prev
 				</button>
 				<button
-					className="playPauseTrack"
+					className="playPauseTrack bg-gray-200 p-2 rounded-md hover:bg-gray-300"
 					onClick={playPauseTrack}>
 					play/pause
 				</button>
 				<button
-					className="nextTrack"
+					className="nextTrack bg-gray-200 p-2 rounded-md hover:bg-gray-300"
 					onClick={nextTrack}>
 					next
 				</button>
