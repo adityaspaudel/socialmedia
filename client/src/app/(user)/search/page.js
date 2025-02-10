cb30856256504823e81a90582dd232b4b1962b1a
Aaditya Paudel <78781844+adityaspaudel@users.noreply.github.com>
2025-02-10 16:03:31 +0545

    user search button updated

--- client/src/app/(user)/search/page.js
+++ client/src/app/(user)/search/page.js
@@ -4,13 +4,12 @@ import React from "react";
 
 const UserSearch = () => {
 	return (
-		<div>
+		<div className="flex gap-2">
 			<SocialMediaSidebarComponent />
 
 			{/*  */}
-			<div className="ml-[200px]">
-				<SearchComponent2 />
-			</div>
+
+			<SearchComponent2 />
 		</div>
 	);
 };
