07e992570f2b3de9a2eaea165fa7fcba92a9a5ed
aaditya paudel <adityaspaudel@gmail.com>
2025-09-25 16:18:13 +0545

    connection issue fixed at index2.js

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const MONGODB_URI=process.env.MONGODB_URI
+const dotenv = require("dotenv");
+dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 // Database connection
 const dbConnect = async () => {
-	try {
-		const isConnected = await mongoose.connect(
-			"mongodb://127.0.0.1:27017/socialmedia"
-		);
-		if (isConnected) console.log("Connected to MongoDB");
-	} catch (err) {
-		console.error("Error connecting to MongoDB:", err);
-	}
+  try {
+    const isConnected = await mongoose.connect(`${MONGODB_URL}/socialmedia`);
+    if (isConnected) console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
 };
 
 module.exports = dbConnect;
--- server/src/index2.js
+++ server/src/index2.js
@@ -6,6 +6,7 @@ const dbConnect = require("./db/connection");
 
 const dotenv = require("dotenv");
 dotenv.config();
+
 // middleware
 app.use(cors());
 app.use(express.json());
