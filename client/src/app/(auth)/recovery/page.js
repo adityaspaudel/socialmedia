3060bd7ce9f7c5c3e3cfb825aecfafa89c8087d1
aaditya paudel <adityaspaudel@gmail.com>
2025-10-05 17:14:33 +0545

    css update

--- client/src/app/(auth)/recovery/page.js
+++ client/src/app/(auth)/recovery/page.js
@@ -1,9 +1,12 @@
 "use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const RecoverAccount = () => {
   const [email, setEmail] = useState("");
+  const router = useRouter();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -11,12 +14,15 @@ const RecoverAccount = () => {
       alert("Please enter a valid email address.");
     } else {
       alert(`Recovery email submitted: ${email}`);
+      router.push("/");
     }
   };
 
   return (
     <div className="flex justify-center items-center bg-green-100 h-screen w-screen">
-      <div className="flex flex-col justify-center items-center gap-4 h-1/2 w-1/2  p-4 bg-gray-100 border-gray-200 border-2 rounded-xl">
+      <div className="flex flex-col justify-center items-center w-full max-w-xl p-6 bg-white rounded-lg shadow-md border-gray-200 border-2">
+        
+          <Link href="/" className="text-start w-full text-2xl">🔙</Link>
         <h2 className="text-2xl font-bold">Recover Account</h2>
         <form onSubmit={handleSubmit} className="flex flex-col gap-2">
           <div className="flex flex-col gap-2">
